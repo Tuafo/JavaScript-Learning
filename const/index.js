d1bc50fb26777e6ab1506533ff5bfdbcdb0eee92
Thiago <thiagomacenogoulart@gmail.com>
2023-01-06 18:44:01 -0300

    master

--- const/index.js
+++ const/index.js
@@ -1,12 +1,14 @@
 // const = a variable that can't be changed
 
-let pi = 3.14159;
+const PI = 3.14159;
 let radius;
 let circumference;
 
 radius = window.prompt("Enter the radius of a circle")
 radius = Number(radius);
 
-circumference = 2 * pi * radius;
+// PI = 360.063;
+
+circumference = 2 * PI * radius;
 
 console.log("The circumference is:", circumference);
---
+++ jsMath/index.js
@@ -0,0 +1,24 @@
+// Math = An intrinsic object that provides basic mathematics functionality and constants.
+
+// let x = 3.14;
+let x;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+x = Math.PI; // take the pi number
+
+// x = Math.round(x); // round a number
+// x = Math.floor(x); // round a number down
+// x = Math.ceil(x); // round a number up
+// x = Math.pow(x, 2); // power of a number
+// x = Math.sqrt(x); // square root of a number
+// x = Math.abs(x); // the distance away from 0
+
+maximum = Math.max(x, y, z)
+minimum = Math.min(x, y, z)
+
+console.log(x)
+console.log(maximum)
+console.log(minimum)
