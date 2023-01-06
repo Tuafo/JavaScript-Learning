2f6e5219a8c5e05673c06ff217de718d7d0037a6
Thiago <thiagomacenogoulart@gmail.com>
2023-01-06 17:04:12 -0300

    master

--- typeConversion/index.js
+++ typeConversion/index.js
@@ -1,3 +1,32 @@
 // Type Conversion = change the datatype of a value to another
 //                   (strings, numbers, booleans)
 
+// a prompt returns a string
+
+// let age = window.prompt("How old are you?");
+
+// so is not possible make a + with this value, because will concatenate
+
+// age += 1;
+/*
+let age = window.prompt("How old are you?");
+console.log(typeof age) // string
+age = Number(age); // converting from string to number
+console.log(typeof age) // number
+
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old")
+*/
+
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("pizza"); // if is empthy returns false
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
