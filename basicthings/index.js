5b89fc78cec7197eda0e08fe75ab0fb2b92560fe
Thiago <thiagomacenogoulart@gmail.com>
2023-01-06 14:03:56 -0300

    master

---
+++ basicthings/index.js
@@ -0,0 +1,15 @@
+//run the html file on live server and inspect the page and open the console log to see the results
+
+console.log("I like pizza!");
+console.log("It's really good!");
+//window.alert("I REALLY LOVE PIZZA!");
+
+// This is a comment
+
+/*
+    This
+    is
+    a
+    multiline
+    comment
+*/ 
