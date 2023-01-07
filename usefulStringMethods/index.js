12e8339780d1b940642df2747a060ee9d3f6bd72
Thiago <thiagomacenogoulart@gmail.com>
2023-01-07 19:59:00 -0300

    master

---
+++ usefulStringMethods/index.js
@@ -0,0 +1,26 @@
+// useful string properties & methods
+
+let userName = "   Thiago Goulart   ";
+let phoneNumber = "(32)9 9129-1231"
+
+// console.log(userName.length);
+
+// let firstLetter = userName.charAt(0); // the character at the index 0
+
+//console.log(userName.charAt(0));
+
+//console.log(userName.indexOf("G")); // the first apperance of a character
+
+//console.log(userName.lastIndexOf("a")); // the last apperance of a character
+
+//userName = userName.trim(); // take all the empthys space before and after
+
+// userName = userName.toUpperCase();
+// userName = userName.toLowerCase();
+
+// phoneNumber= phoneNumber.replaceAll("(", "[")
+// phoneNumber= phoneNumber.replaceAll(")", "]")
+// phoneNumber= phoneNumber.replaceAll("-", "")
+
+// console.log(userName);
+// console.log(phoneNumber);
