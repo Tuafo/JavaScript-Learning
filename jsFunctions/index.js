594b1da1fd35c7f551997e5840a0c97a94fe2fbb
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 20:45:35 -0300

    master

---
+++ jsFunctions/index.js
@@ -0,0 +1,19 @@
+// function = define code once, and use it many times.
+//            To perform some code, call the function name.
+
+startProgram();
+
+function startProgram() {
+    let userName = "Thiago";
+    let age = 20;
+    
+    happyBirthday(userName, age);
+}
+
+function happyBirthday(userName, age) {
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", userName);
+    console.log("Happy birthday to you!");
+    console.log("You are", age, "years old!");
+}
