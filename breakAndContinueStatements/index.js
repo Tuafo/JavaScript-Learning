4b2a69b9e0ae2584cd257635c78aeec2030dc58e
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 15:56:42 -0300

    master

---
+++ breakAndContinueStatements/index.js
@@ -0,0 +1,9 @@
+// break = breaks out of a loop entirely
+// continue = skip an interation of a loop
+
+for(let i = 1; i <= 20; i++){
+    if(i == 17){
+        continue;
+    }
+    console.log(i);
+}
