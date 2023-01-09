f49f439b14b32f65108cc48b16819da634711ced
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 12:47:16 -0300

    master

---
+++ ifStatements/index.js
@@ -0,0 +1,29 @@
+// if statement = a basic form of decision making
+//                if a condition is true, then do something
+//                if not, the don't do it!
+/*
+let age = 20;
+
+if(age >= 65){
+    console.log("You are a senior citizen!")
+}
+
+else if(age < 0){
+    console.log("YOU HAVEN'T BEEN BORN YET!")
+}
+else if(age >= 18){
+    console.log("You are an adult!");
+}
+else{
+    console.log("You are a child!")
+}
+*/
+
+let online = true;
+
+if(online){
+    console.log("You are online!")
+}
+else{
+    console.log("You are offline!")
+}
