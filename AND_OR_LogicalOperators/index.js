40d6b6d3e8ff943eb3d523a8f8557ebf2c592cc9
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 14:45:04 -0300

    master

---
+++ AND_OR_LogicalOperators/index.js
@@ -0,0 +1,33 @@
+// Give us the ability to check more than 1 condition concurrently
+// && AND (BOTH conditions must be true)
+// || OR (Either condition can be true)
+
+let temp = 15;
+let sunny = true;
+
+/*
+if(temp > 0 && temp < 30){
+    console.log("The weather is GOOD!")
+}
+else{
+    console.log("The weather is BAD!")
+}
+*/
+
+/*
+if(temp <= 0 || temp >= 30){
+    console.log("The weather is BAD!")
+}
+else{
+    console.log("The weather is GOOD!")
+}
+*/
+
+/*
+if(temp > 0 && temp < 30 && sunny){
+    console.log("The weather is GOOD!")
+}
+else{
+    console.log("The weather is BAD!")
+}
+*/
