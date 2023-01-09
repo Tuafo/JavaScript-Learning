593551d25e80dd201efff2842be76c51470a78f4
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 12:26:06 -0300

    master

---
+++ methodChaining/index.js
@@ -0,0 +1,9 @@
+// method chaining = calling one method after another
+//                   in one continuous line of code
+
+let userName = "thiago";
+
+let letter = userName.charAt(0).toUpperCase();
+// letter = letter.toUpperCase();
+
+console.log(letter);
