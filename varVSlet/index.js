8eab6a416076c150fbcebee741a046735fb27c81
Thiago <thiagomacenogoulart@gmail.com>
2023-01-09 21:30:24 -0300

    master

---
+++ varVSlet/index.js
@@ -0,0 +1,27 @@
+// variable scope = where a variable is accessible
+
+// let = variables are limited to block scope {}
+// var = variables are limited to a function(){}
+
+// global variable = is declared outside any function
+// (if gloabl, var will CHANGE browser's window properties)
+
+/*
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+}
+*/
+/*
+let name = "Thiago";
+
+doSomething();
+
+function doSomething() { 
+    for (var i = 1; i <= 3; i++) {
+        console.log(i);
+    }
+}
+
+console.log(i);
+*/
+
