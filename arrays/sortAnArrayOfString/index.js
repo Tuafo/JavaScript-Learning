3ff9437042284879252e7a886d5d2756a7fc6785
Thiago <thiagomacenogoulart@gmail.com>
2023-01-23 14:31:44 -0300

    master

---
+++ arrays/sortAnArrayOfString/index.js
@@ -0,0 +1,9 @@
+
+let fruits = ["banana", "apple", "orange", "mango"];
+
+// fruits = fruits.sort(); // order by alphabetic order
+// fruits = fruits.sort().reverse(); // reverted alphabetic order
+
+for(let fruit of fruits) {
+    console.log(fruit);
+}
