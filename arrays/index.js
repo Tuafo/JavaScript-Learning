76951d9a9a36570566b0b00b90d928a1ea19daba
Thiago <thiagomacenogoulart@gmail.com>
2023-01-23 14:16:35 -0300

    master

---
+++ arrays/index.js
@@ -0,0 +1,18 @@
+// array = think of it as a variable
+//         that can store multiple values
+
+let fruits = ["apple", "orange", "banana"];
+
+// fruits[3] = "coconut";
+// fruits.push("lemon");    // add an element
+// fruits.pop();            // removes last element
+// fruits.unshift("mango"); // add element to beginning
+// fruits.shift();          // removes element from beginning
+
+// let length = fruits.length;
+// let index = fruits.indexOf("orange")
+
+// console.log(fruits[0]);
+// console.log(fruits);
+// console.log(index);
+
